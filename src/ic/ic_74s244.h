83061efa77c33899cd6a810c9804e5bf00b478d1
Dean Beeler <canadacow@gmail.com>
2026-03-24 17:35:13 -0700

    4.8mhz

--- src/ic/ic_74s244.h
+++ src/ic/ic_74s244.h
@@ -35,18 +35,72 @@ namespace bench {
 //   Pin 20: VCC
 //
 // Behavior:  Y = A when ~G Low,  Y = HiZ when ~G High.
+//   TTL: floating/HiZ input treated as High -> output High.
+//   Buffer formula: level | (level - 1)
 //
 // Threading: CallbackComponent -- combinational, no fiber.
 class IC_74S244 : public CallbackComponent {
 public:
-    IC_74S244();
+    IC_74S244() : CallbackComponent("74S244") { set_description("Octal Buffer"); }
 
-    void install(Socket& socket);
+    void install(Socket& socket) {
+        auto connect_pin = [&](int p) -> Pin {
+            Signal* s = socket.pin_signal(p);
+            if (s) s->connect(this);
+            return s ? s->pin() : Pin{};
+        };
+        auto pin = [&](int p) -> Pin {
+            Signal* s = socket.pin_signal(p);
+            return s ? s->pin() : Pin{};
+        };
+
+        // Enable pins
+        pin_g1_ = connect_pin(1);   // ~1G (group 1 enable, active low)
+        pin_g2_ = connect_pin(19);  // ~2G (group 2 enable, active low)
+
+        // Group 1: inputs (2,4,6,8) -> outputs (18,16,14,12)
+        grp1_[0].a = connect_pin(2);  grp1_[0].y = pin(18);  // 1A1 -> 1Y1
+        grp1_[1].a = connect_pin(4);  grp1_[1].y = pin(16);  // 1A2 -> 1Y2
+        grp1_[2].a = connect_pin(6);  grp1_[2].y = pin(14);  // 1A3 -> 1Y3
+        grp1_[3].a = connect_pin(8);  grp1_[3].y = pin(12);  // 1A4 -> 1Y4
+
+        // Group 2: inputs (11,13,15,17) -> outputs (9,7,5,3)
+        grp2_[0].a = connect_pin(11); grp2_[0].y = pin(9);   // 2A1 -> 2Y1
+        grp2_[1].a = connect_pin(13); grp2_[1].y = pin(7);   // 2A2 -> 2Y2
+        grp2_[2].a = connect_pin(15); grp2_[2].y = pin(5);   // 2A3 -> 2Y3
+        grp2_[3].a = connect_pin(17); grp2_[3].y = pin(3);   // 2A4 -> 2Y4
+
+        // VCC
+        Signal* vcc = socket.pin_signal(20);
+        if (vcc) vcc->connect(this);
+
+        // ~G1 and ~G2 feed bidir lambdas (sampled at permutation time), async.
+        declare_input(pin_g1_);
+        declare_input(pin_g2_);
+        for (auto& b : grp1_) { declare_input(b.a); declare_input(b.y); declare_output(b.y); }
+        for (auto& b : grp2_) { declare_input(b.a); declare_input(b.y); declare_output(b.y); }
+    }
 
 protected:
-    void on_power_on() override;
-    void on_power_off() override;
-    void on_cycle(Fiber caller) override;
+    void on_power_on() override { }
+    void on_power_off() override { }
+
+    void on_cycle(Fiber /*caller*/) override {
+        if (pin_g1_.level() == Level::Low) {
+            int8_t v;
+            v = (int8_t)grp1_[0].a.level(); grp1_[0].y.drive((Level)(v | (v - 1)));
+            v = (int8_t)grp1_[1].a.level(); grp1_[1].y.drive((Level)(v | (v - 1)));
+            v = (int8_t)grp1_[2].a.level(); grp1_[2].y.drive((Level)(v | (v - 1)));
+            v = (int8_t)grp1_[3].a.level(); grp1_[3].y.drive((Level)(v | (v - 1)));
+        }
+        if (pin_g2_.level() == Level::Low) {
+            int8_t v;
+            v = (int8_t)grp2_[0].a.level(); grp2_[0].y.drive((Level)(v | (v - 1)));
+            v = (int8_t)grp2_[1].a.level(); grp2_[1].y.drive((Level)(v | (v - 1)));
+            v = (int8_t)grp2_[2].a.level(); grp2_[2].y.drive((Level)(v | (v - 1)));
+            v = (int8_t)grp2_[3].a.level(); grp2_[3].y.drive((Level)(v | (v - 1)));
+        }
+    }
 
 private:
 
